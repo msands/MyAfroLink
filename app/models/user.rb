b4fd459b1a38bf19e26b642f55318a7b219372c9
Marcel Sands <marcel.sands84@gmail.com>
2015-05-16 21:04:17 -0400

    add associations to users and profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :confirmable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
+  has_one :user_profile, dependent: :destroy
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,2 +1,5 @@
 class UserProfile < ActiveRecord::Base
+  belongs_to :user
+
+  validates :first_name, :last_name, presence: true
 end
