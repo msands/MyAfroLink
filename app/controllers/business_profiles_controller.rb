7672d00a07bf55f9d22385b36e57252fdf8f665a
Marcel Sands <marcel.sands84@gmail.com>
2015-06-01 00:49:00 -0400

    add logic to business controller

--- app/controllers/business_profiles_controller.rb
+++ app/controllers/business_profiles_controller.rb
@@ -1,21 +1,42 @@
 class BusinessProfilesController< ApplicationController
 
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @business_profiles = BusinessProfile.all
   end
 
   def show
+    @business_profile = BusinessProfile.find(params[:id])
   end
 
   def new
+    @business_profile = BusinessProfile.new
   end
 
   def create
+    @business_profile = BusinessProfile.new(business_profile_params)
+    @business_profile.user = current_user
+    if @business_profile.save
+      redirect_to business_profile_path, notice: 'Business Profile was successfully listed'
+    else
+      flash[:error] = 'There was an error listing your business'
+      render 'new'
+    end
   end
 
   def edit
+    @business_profile = BusinessProfile.find(params[:id])
   end
 
   def update
+    @business_profile = BusinessProfile.find(params[:id])
+    if @business_profile.update_attributes(business_profile_params)
+      flash[:success] = "Business Profile updated"
+      redirect_to business_profile_path
+    else
+      render 'edit'
+    end
   end
 
   private
