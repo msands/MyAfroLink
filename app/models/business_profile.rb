25e2c0654f95bfd82d33d60f72728f6e016ec7fb
Marcel Sands <marcel.sands84@gmail.com>
2015-06-07 17:04:14 -0400

    add placeholders to business profile form views

--- app/models/business_profile.rb
+++ app/models/business_profile.rb
@@ -6,5 +6,5 @@ class BusinessProfile < ActiveRecord::Base
   has_attached_file :image, :styles => { large: "600x600>", medium: "400x400>", small: "200x200>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  validates :name, :address, :city, :time_open, :time_close,:category, presence: true
+  validates :name, :address, :city, :category, presence: true
 end
