f61e89ff15b5d9b34630830762b151c3b0a72328
Marcel Sands <marcel.sands84@gmail.com>
2015-06-20 20:40:43 -0400

    update categories for User Tribes and Business Categories

--- app/models/business_profile.rb
+++ app/models/business_profile.rb
@@ -1,7 +1,7 @@
 class BusinessProfile < ActiveRecord::Base
   searchkick
 
-  CATEGORIES = ["Food", "Jewelry", "Clothing", "Grocery"]
+  CATEGORIES = ["ELECTRONICS", "Home Electronics", "TV, DVD, LCDs", "Mobile & Tablets", "Technical Services", "EVENTS & NOTICES", "Auction Announcements", "Business Events", "Business Notices", "Children Events", "Legal & Public Notices", "Personal", "FASHION", "Bags", "Beauty Products", "Clothing M/F", "Jewelry", "Shoes M/F", "Watches", "HEALTH & FITNESS", "Activities", "Fitness", "Gym", "Hospitals & Clinics", "Parlours", "Saloons", "HOME & LIFESTYLE", "Art & Photography", "Entertainment", "Fittings", "Home Furniture", "Office Furniture", "Used Things", "JOBS", "Accounts Jobs", "Cleaning & Washing", "Data Entry", "Design & Code", "Finance Jobs", "Marketing Jobs", "MATRIMONIALS", "Brides", "Casual Dating", "Dating", "Friendship", "Grooms", "Open Relationship", "MISCELLANEOUS", "Classic & Antique", "Kids Stuff", "Music Equipment", "Pets Stuff", "Scrape", "Second Hand", "Farms", "Home for rent", "Hotels", "Land for sale", "Offices for rent", "Shops for rent", "SERVICES", "Cleaning Services", "Educational", "Food Services", "Medical", "Office & Home Removals", "Technical", "VEHICLES", "Cars & Modern", "Fancy Cars", "Kids Bikes", "Motor Bikes", "Vans & Truck"]
 
   belongs_to :user
 
