94f9ea0505f7eca73d8c636ffe04ef16e294710d
Marcel Sands <marcel.sands84@gmail.com>
2015-11-04 13:04:27 -0500

    run devise_invitable installation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :confirmable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
   has_one :user_profile, dependent: :destroy
