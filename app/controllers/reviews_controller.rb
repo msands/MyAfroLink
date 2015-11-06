c0dfc47583eadda769520b2cd9fbaa5c6fcdbba7
Marcel Sands <marcel.sands84@gmail.com>
2015-11-06 14:34:56 -0500

    update new and private actions for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_filter :load_reviewable
+
   def index
     @reviewable = BusinessProfile.find(params[:business_profile_id])
     @reviews = @reviewable.reviews
@@ -6,4 +8,16 @@ class ReviewsController < ApplicationController
 
   def new
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+
+  def load_reviewable
+    resource, id = request.path.split('/')[1,2]
+    @reviewable = resource.singularize.classify.constantize.find(id)
+  end
+
 end
